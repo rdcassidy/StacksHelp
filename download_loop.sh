98f6c7a07163b3a511a2dd00dc2cfc75d4ff3c39
rdcassidy <127222031+rdcassidy@users.noreply.github.com>
2024-04-26 12:04:52 +0200

    Update download_loop.sh

--- download_loop.sh
+++ download_loop.sh
@@ -1,11 +1,20 @@
 #!/bin/sh
-#download loop 
-taxa='Pgrayi'
 
-for i in {3..7}
-do
-mkdir ${taxa}_m$i
+STACKS="/mbl/share/workspaces/groups/riesgo/Sergi/Emuelleri/Stacks" ### directory to the output folders from optimization (probably stacks)
+Param='M' #parameter of outputs as it is put in the folder names (m, M or N)
+Downloads="/mbl/share/workspaces/groups/riesgo/Sergi/Emuelleri/Downloads" #path to folder where you will store downloads. make this if you don't have it already
+files="Emue_Subset1
+Emue_Subset2
+Emue_Subset3
+Emue_Subset4
+Emue_Subset5"
+#names of the taxa/populations. for example, for the output folder Pgrayi_m3, put Pgrayi.
 
-scp -o 'ProxyJump sert@orca.nhm.ac.uk' sert@franklin:/mbl/share/workspaces/groups/riesgo/Sergi/Ana-Torriente/Stacks/${taxa}_m$i/population* ${taxa}_m$i/
-
-done 
+for sample in $files; do
+    for folder in "${STACKS}/${sample}_${Param}"*; do
+        cp "$folder"/populations.snps.vcf "${Downloads}/"
+        echo "Copying populations.snps.vcf file from ${sample}_${Param}*"
+        mv "${Downloads}/populations.snps.vcf" "${Downloads}/$(basename "$folder").vcf"
+        echo "renaming populations.snps.vcf file to $(basename "$folder").vcf"
+    done
+done
